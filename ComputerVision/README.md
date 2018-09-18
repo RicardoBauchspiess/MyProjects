3d14012f5b2a5c3bf14365f6b94576e63bd50dab
Ricardo Bauchspiess <ricardobauchspiess@gmail.com>
2018-09-18 01:44:38 -0300

    Adicionada fusão por filtro de Kalman Extendido

--- ComputerVision/README.md
+++ ComputerVision/README.md
@@ -1,7 +1,32 @@
-V1.0:  
-Rastreamento de um objeto definido pelo usuÃ¡rio utilizando SURF e Brute Force Matcher  
-Clica e arrasta sobre a imagem permite ao usuÃ¡rio selecionar template do objeto a ser rastreado (linhas verdes)  
-Algoritmo SURF (Speeded-Up Robust Features) obtÃ©m descritores do template salvo e do frame mais recente  
-Algoritmo Brute Force Matcher obtÃ©m correspondÃªncia entre descritores do template e do frame  
-Homografia obtÃ©m transformaÃ§Ã£o entre orientaÃ§Ã£o e dimensÃ£o do objeto no frame e no template  
-Aplica-se transformada obtida ao retÃ¢ngulo limite do template e desenha o retÃ¢ngulo transformado sobre a imagem (linhas azuis)  
+V2.0:  
+Resumo:  
+Identificação do objeto utilizando SURF (Speeded-Up Robust Features)+Brute Force Matches  
+Rastreamento do objeto utilizando rastreador CSRT (Discriminative Correlation Filter with Channel and Spatial Reliability)  
+Fusão do identificador e do rastreador utilizando Filtro de Kalman Extendido (EKF)    
+
+
+Algoritmo:
+-Inicia matrizes do EKF
+Inicia matrizes de confiança do modelo Q e da medição R
+Matriz de relação entre medição e estados H=Identidade, portanto não aparece nas equações
+-Inicialização
+Usuário: define objeto (botões 1 e 2) e seleciona região do objeto
+Inicializa estado (X=(x1,y1,x2,y2)'), com seleção do usuário e matriz de covariâncias P
+Inicia rastreador com região selecionada
+Obtém pontos chaves e descritores da região selecionada
+-Etapa de predição do EKF
+Predição da região nova (X_k+1|k) obtida pelo rastreador CSRT
+Atualização da matriz de covariâncias P = F*P*F'+Q com F = k*diagonal(dx1,dy1,dx2,dy2)+I, sendo dn a variação da variável de estado n 
+-Etapa de atualização do EKF
+SURF+Brute Force Matcher obtém correspondência entre pontos da imagem e pontos do template inicial
+Não havendo correspondências suficientes segue pela predição
+Havendo correspondências suficientes carrega matriz de medida e obtém matriz de diferença entre medição e predição
+y = (x1,y1,x2,y2) medidos, q=y-X_k+1|k
+Calcula-se a distância de Mahalanobis para a medição (q*inv(P+R)*q')
+Se a distância estiver abaixo de 9,488 (confiabilidade de 95% para 4 graus de liberdade na tabela chi quadrado) a medição é aceita
+do contrário segue apenas pela predição
+Se a medição for aceita atualiza matriz de estado e de covariâncias
+K = P*inv(P+R)
+X_k+1|k+1 = X_k+1|k+K*q
+P = (I-K)*P*(I-K)'+K*R*K'
+Reajusta rastreador para a nova região
--- ComputerVision/Tracking.py
+++ ComputerVision/Tracking.py
@@ -8,9 +8,9 @@ import os
 
 import numpy as np
 import cv2
-#from matplotlib import pyplot as plt
 
-os.environ["OPENCV_VIDEOIO_PRIORITY_MSMF"] = "0"
+
+os.environ["OPENCV_VIDEOIO_PRIORITY_MSMF"] = "0" #Garante que webcam desliga por completo
 
 #função de retorno do mouse para obter templates, grava os pontos inicial e final da região desejada
 point = []
@@ -35,59 +35,57 @@ def click_and_crop(event, x, y, flags, param):
         point[1] = (x, y)
         
 
-cont = 5;
-template_num = 0;
+#parametros de controle
 template1 = False
 template2 = False
-
-pred1 = [[0,0],[0,0]]
-pred2 = [[0,0],[0,0]]
-
-font = cv2.FONT_HERSHEY_SIMPLEX
+newframe = True
+template_num = 0
 
 #captura da webcam
 video_capture = cv2.VideoCapture(0)
 
-#grava video
+#Inicia gravador de video
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 video_writer = cv2.VideoWriter("output.avi", fourcc, 15, (640, 480))
 
-newframe = True
+#Inicia seletor de ROI
 cv2.namedWindow("Video")
 cv2.setMouseCallback("Video", click_and_crop)
 
-# Inicia detectores SIFT ou SURF
+font = cv2.FONT_HERSHEY_SIMPLEX
 
-#sift = cv2.xfeatures2d.SIFT_create()
+#---------------Parâmetros Detector de Features
+#Inicia SURF
 surf = cv2.xfeatures2d.SURF_create(400)
 
 #Inicia matcher brute force
-MIN_MATCH_COUNT = 5
+MIN_MATCH_COUNT = 10
 bf = cv2.BFMatcher()
 
-initBB = None
-
-global gray_roi1, gray_roi2, kp1, kp2
 
 
-
-
-#Create surf for
-"""
-img = cv2.imread("1.jpg")
-cv2.imshow("img1",img)
-gray_roi1 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-kp1, des1 = surf.detectAndCompute(img,None)
-h1,w1 = gray_roi1.shape
-Template1 = True
-"""
+#----------Parâmetros para filtro de Kalman
+# Matriz de estados X limites da região rastreada: [xmin, ymin, xmax, ymax]'
+# Matriz X inicializada pela primeira ROI selecionada pelo usuário
+# f é função não linear de predição do alforitmo de rastremento
+# X_k+1|k obtém valores diretamento do alforitmo de rastremento
+# F será matriz diagonal com a variação de cada variável de estado
+# P = Matriz de covariâncias, baixo valor inicial pois a região inicial é entrada do usuário
+# Q = matriz de confiabilidade do modelo
+Q = np.matrix( ((1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,0,1)) )
+Q2 = np.matrix( ((1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,0,1)) )
+# y = medição realizada pelo algoritmo de detecção por feature matching
+# matriz H da medição é matriz identidade
+# R = matriz de confiabilidade da medição, valor mais elevado devido às imprecisões do algoritmo de matching
+R = np.matrix( ((100,0,0,0),(0,100,0,0),(0,0,10,0),(0,0,0,100)) )
+R2 = np.matrix( ((100,0,0,0),(0,100,0,0),(0,0,10,0),(0,0,0,100)) )
 
 
 
 while True:
-    #pega novo frame da camera
+    #pega novo frame da camera se não estiver em pausa
     if newframe:
-        _, frame = video_capture.read() # We get the last frame.
+        _, frame = video_capture.read() 
     temp_image = frame.copy()
 
     W,H = frame.shape[:2]
@@ -96,85 +94,116 @@ while True:
         cv2.rectangle(temp_image, point[0], point[1], (0, 255, 0), 2)
     if template1 == True or template2 == True:
         #converte imagem para tons de cinza e obtém os pontos chaves e descritores do último frame
-        #gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #kp, des = surf.detectAndCompute(gray_image,None)
-        #kp2, des2 = sift.detectAndCompute(gray_image,None)                
+        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        kp, des = surf.detectAndCompute(gray_image,None)
+                   
         if template1 == True:
-            #Predição
+            #Etapa de predição do EKF
             (success, box) = tracker1.update(frame)
             if success:
                 (x, y, w, h) = [int(v) for v in box]
-                cv2.rectangle(temp_image, (x, y), (x + w, y + h),(255, 255,0), 2)
-            xmin = int(max(x-0.1*w,0))
-            xmax = int(min(x+0.1*w,W))
-            ymin = int(max(y-0.1*h,0))		
-            ymax = int(min(y+0.1*h,H))
-            #roi = frame[ymin:ymax, xmin:xmax].copy()
-            gray_roi = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) #converte imagem para tons de cinza
-            #Encontra pontos chave e descritores do template com SIFT ou SURF
-            kp, des = surf.detectAndCompute(gray_roi,None)
+                #cv2.rectangle(temp_image, (x, y), (x + w, y + h),(255, 255,0), 2)
+                X_ant = X[:,:]            
+                X = np.matrix(((x),(y),(x+w),(y+h))).transpose()            
+                dx1 = np.abs(X[0,0]-X_ant[0,0])/10+1
+                dy1 = np.abs(X[1,0]-X_ant[1,0])/10+1
+                dx2 = np.abs(X[2,0]-X_ant[2,0])/10+1
+                dy2 = np.abs(X[3,0]-X_ant[3,0])/10+1
+                F = np.matrix( (( dx1,0,0,0),(0,dy1,0,0),(0,0,dx2,0),(0,0,0,dy2)) )                
+                P = F*P*F.transpose()+Q
             
-                
+            #Etapa de Atualização do EKF
+            
+            #Obtém matches entre pontos chave
             matches = bf.knnMatch(des1, des, k=2)
-             #Salva as boas correspondências
+            #Salva as boas correspondências
             good = []
             for m,n in matches:
                 if m.distance < 0.7*n.distance:
                     good.append(m)
-            
+            """
+            #Desenha matches
             kt = []
             for m in good:
                 kt.append(kp[m.trainIdx])
             temp_image = cv2.drawKeypoints(temp_image, kt,None,(255,0,0),4)
+            """
             if len(good)>MIN_MATCH_COUNT:
                 #obtém pontos equivalentes entre template e frame
                 src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
                 dst_pts = np.float32([ kp[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-                
-                
-            
                 #obtém transformação entre pontos do template e do frame
                 M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
                 if mask is not None and M is not None:
                     matchesMask = mask.ravel().tolist()
                 
-                    #transforma retângulo do template em região equivalente no frame e desenha quadrilátero
+                    #transforma retângulo do template em região equivalente no frame
                     pts = np.float32([ [0,0],[0,h1-1],[w1-1,h1-1],[w1-1,0] ]).reshape(-1,1,2)
                     dst = cv2.perspectiveTransform(pts,M)
                     #temp_image = cv2.polylines(temp_image,[np.int32(dst)],True,(255,0,0),3, cv2.LINE_AA)
-         
-                    cv2.putText(temp_image,"1",(dst[3,0,0],dst[3,0,1]), font, 0.8,(255,0,0),2,cv2.LINE_AA)
                     
+                    #obtém retângulo médio entre os pontos obtidos como medição do algoritmo
                     x_min = max(np.int32( (dst[0,0,0]+dst[1,0,0])/2 ),0)
                     y_min = max(np.int32( (dst[0,0,1]+dst[3,0,1])/2  ),0)
                     x_max = np.int32( (dst[2,0,0]+dst[3,0,0])/2 )
                     y_max = np.int32( (dst[1,0,1]+dst[2,0,1])/2 )
-                    if x_min != x_max and y_min != y_max:
-                        cv2.rectangle(temp_image, (x_min,y_min), (x_max,y_max), (255,0,0), 2)
-                                                                    
+                    #if x_min != x_max and y_min != y_max:
+                        #cv2.rectangle(temp_image, (x_min,y_min), (x_max,y_max), (255,0,0), 2)
+                    
+                    #obtém medição e diferença entre medição e predição
+                    y = np.matrix(((x_min),(y_min),(x_max),(y_max))).transpose()                
+                    q = y - X
+                    #calcula distancia de Mahalanobis
+                    Minv = np.linalg.inv(R+P)      
+                    Mdist = q.transpose()*Minv*q
+                    #Se a distancia de Mahalanobis estiver abaixo do valor de confiabilidade 95%
+                    #para 4 graus de liberdade, a medição é aceita
+                    if Mdist < 9.488:
+                        #Atualiza estado e matriz de covariâncias
+                        K = P*Minv
+                        X = X+K*q
+                        P = (np.eye(4)-K)*P*(np.eye(4)-K).transpose()+K*R*K.transpose()
+                        #Ajusta rastreador para nova posição
+                        if X[0]<X[2] and X[1]<X[3]:
+                            initBB = (X[0],X[1], X[2]+1-X[0],X[3]+1-X[1])
+                            tracker1 = cv2.TrackerCSRT_create()
+                            tracker1.init(frame, initBB)
+            #Desenha retangulo na região definida
+            cv2.putText(temp_image,"1",(np.int(X[0,0]),np.int(X[1,0])), font, 0.8,(255,0,0),2,cv2.LINE_AA)      
+            cv2.rectangle(temp_image, (np.int(X[0,0]),np.int(X[1,0])), (np.int(X[2,0]),np.int(X[3,0])), (255,0,0), 2)
+                                                                                                        
 
         if template2 == True:
+            #Etapa de predição do EKF
             (success, box) = tracker2.update(frame)
             if success:
                 (x, y, w, h) = [int(v) for v in box]
-                cv2.rectangle(temp_image, (x, y), (x + w, y + h),(0,255, 255), 2)
+                #cv2.rectangle(temp_image, (x, y), (x + w, y + h),(0,255, 255), 2)
+                X_ant = X2[:,:]            
+                X2 = np.matrix(((x),(y),(x+w),(y+h))).transpose()            
+                dx1 = np.abs(X2[0,0]-X_ant[0,0])/10+1
+                dy1 = np.abs(X2[1,0]-X_ant[1,0])/10+1
+                dx2 = np.abs(X2[2,0]-X_ant[2,0])/10+1
+                dy2 = np.abs(X2[3,0]-X_ant[3,0])/10+1
+                F = np.matrix( (( dx1,0,0,0),(0,dy1,0,0),(0,0,dx2,0),(0,0,0,dy2)) )                
+                P2 = F*P2*F.transpose()+Q2
             
-            gray_roi = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) #converte imagem para tons de cinza
-            #Encontra pontos chave e descritores do template com SIFT ou SURF
-            kp, des = surf.detectAndCompute(gray_roi,None)
+            #Etapa de Atualização do EKF
             
-            
+            #Obtém matches entre pontos chave           
             matches = bf.knnMatch(des2, des, k=2)
-             #Salva as boas correspondências
+            #Salva as boas correspondências
             good = []
             for m,n in matches:
                 if m.distance < 0.7*n.distance:
                     good.append(m)
-            
+            """
+            #Desenha matches
             kt = []
             for m in good:
                 kt.append(kp[m.trainIdx])
             temp_image = cv2.drawKeypoints(temp_image, kt,None,(0,0,255),4)
+            """
             if len(good)>MIN_MATCH_COUNT:
                 #obtém pontos equivalentes entre template e frame
                 src_pts = np.float32([ kp2[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
@@ -185,22 +214,40 @@ while True:
                 if mask is not None and M is not None:
                     matchesMask = mask.ravel().tolist()
                 
-                    #transforma retângulo do template em região equivalente no frame e desenha quadrilátero
-                    h,w = gray_roi2.shape
-                    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+                    #transforma retângulo do template em região equivalente no frame
+                    pts = np.float32([ [0,0],[0,h2-1],[w2-1,h2-1],[w2-1,0] ]).reshape(-1,1,2)
                     dst = cv2.perspectiveTransform(pts,M)
                     #temp_image = cv2.polylines(temp_image,[np.int32(dst)],True,(0,0,255),3, cv2.LINE_AA)
-                    cv2.putText(temp_image,'2',(dst[0,0,0],dst[0,0,1]), font, 0.8,(0,0,255),2,cv2.LINE_AA)
-                    
-                    
+                                      
+                    #obtém retângulo médio entre os pontos obtidos como medição do algoritmo
                     x_min = np.int32( (dst[0,0,0]+dst[1,0,0])/2 )
                     y_min = np.int32( (dst[0,0,1]+dst[3,0,1])/2  )
                     x_max = np.int32( (dst[2,0,0]+dst[3,0,0])/2 )
                     y_max = np.int32( (dst[1,0,1]+dst[2,0,1])/2 )
-                    if x_min != x_max and y_min != y_max:
-                        cv2.rectangle(temp_image, (x_min,y_min), (x_max,y_max), (0,0,255), 2)
+                    #if x_min != x_max and y_min != y_max:
+                        #cv2.rectangle(temp_image, (x_min,y_min), (x_max,y_max), (0,0,255), 2)
+                        
+                    #obtém medição e diferença entre medição e predição
+                    y = np.matrix(((x_min),(y_min),(x_max),(y_max))).transpose()                
+                    q = y - X2
+                    #calcula distancia de Mahalanobis
+                    Minv = np.linalg.inv(R2+P2)      
+                    Mdist = q.transpose()*Minv*q
+                    #Se a distancia de Mahalanobis estiver abaixo do valor de confiabilidade 95%
+                    #para 4 graus de liberdade, a medição é aceita
+                    if Mdist < 9.488:
+                        #Atualiza estado e matriz de covariâncias
+                        K = P2*Minv
+                        X2 = X2+K*q
+                        P2 = (np.eye(4)-K)*P2*(np.eye(4)-K).transpose()+K*R2*K.transpose()
+                        #Ajusta rastreador para nova posição
+                        if X2[0]<X2[2] and X2[1]<X2[3]:
+                            initBB = (X2[0],X2[1], X2[2]+1-X2[0],X2[3]+1-X2[1])
+                            tracker2 = cv2.TrackerCSRT_create()
+                            tracker2.init(frame, initBB)
                     
-                    
+            cv2.putText(temp_image,'2',(np.int(X2[0,0]),np.int(X2[1,0])), font, 0.8,(0,0,255),2,cv2.LINE_AA)
+            cv2.rectangle(temp_image, (np.int(X2[0,0]),np.int(X2[1,0])), (np.int(X2[2,0]),np.int(X2[3,0])), (0,0,255), 2)
     if done == True and (point[0][1] != point[1][1] or point[0][0] != point[1][0]):
         #Se tiver a região selecionada pelo mouse salva template
         done = False
@@ -220,27 +267,38 @@ while True:
                 roi = frame[point[1][1]:point[0][1]+1, point[1][0]:point[0][0]+1]
                 initBB = (point[1][0],point[1][1], point[0][0]+1-point[1][0],point[0][1]+1-point[1][1])
                 
-        if template_num == 1:      
-            cv2.imshow('ROI1', roi) #mostra template atual
+        if template_num == 1:
+            template1 = True;
+            #cv2.imshow('ROI1', roi) #mostra template atual
             gray_roi1 = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY) #converte imagem para tons de cinza
             #Encontra pontos chave e descritores do template com SIFT ou SURF
             kp1, des1 = surf.detectAndCompute(gray_roi1,None)
             h1,w1 = gray_roi1.shape
-            #kp1, des1 = sift.detectAndCompute(gray_roi,None)
-            template1 = True;
+            
+            #Inicializa rastreador
             tracker1 = cv2.TrackerCSRT_create()
             tracker1.init(frame, initBB)
             
+            #inicialização do filtro de Kalman estado X e matriz de covariancia P
+            X = np.matrix(((initBB[0]),(initBB[1]),(initBB[2]+initBB[0]-1),(initBB[3]+initBB[1]-1))).transpose()
+            P = np.matrix( ((1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,0,1)) )
+
+            
         elif template_num == 2:
-            cv2.imshow('ROI2', roi) #mostra template atual
+            template2 = True;
+            #cv2.imshow('ROI2', roi) #mostra template atual
             gray_roi2 = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY) #converte imagem para tons de cinza
             #Encontra pontos chave e descritores do template com SIFT ou SURF
             kp2, des2 = surf.detectAndCompute(gray_roi2,None)
             h2,w2 = gray_roi2.shape
-            #kp1, des1 = sift.detectAndCompute(gray_roi,None)
-            template2 = True;
+            
+            #inicializa rastreador
             tracker2 = cv2.TrackerCSRT_create()
             tracker2.init(frame, initBB)
+            
+            #inicialização do filtro de Kalman estado X e matriz de covariancia P
+            X2 = np.matrix(((initBB[0]),(initBB[1]),(initBB[2]+initBB[0]-1),(initBB[3]+initBB[1]-1))).transpose()
+            P2 = np.matrix( ((1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,0,1)) )
             
 
     #mostra imagem na tela e salva em video
